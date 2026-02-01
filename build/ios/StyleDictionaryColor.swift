61202e1969e438bf57249d6fb00ee1083d01c804
Ehsan <vaeghi@gmail.com>
2026-02-01 14:15:36 +0330

    Setup Style Dictionary with Android/iOS support and correct token structure

---
+++ build/ios/StyleDictionaryColor.swift
@@ -0,0 +1,37 @@
+
+//
+// StyleDictionaryColor.swift
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+import UIKit
+
+public class StyleDictionaryColor {
+    public static let primitiveColorBlack = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let primitiveColorBlue = UIColor(red: 0.000, green: 0.000, blue: 1.000, alpha: 1)
+    public static let primitiveColorCyan = UIColor(red: 0.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let primitiveColorGray = UIColor(red: 0.502, green: 0.502, blue: 0.502, alpha: 1)
+    public static let primitiveColorGreen = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
+    public static let primitiveColorMagenta = UIColor(red: 1.000, green: 0.000, blue: 1.000, alpha: 1)
+    public static let primitiveColorMaroon = UIColor(red: 0.502, green: 0.000, blue: 0.000, alpha: 1)
+    public static let primitiveColorNavy = UIColor(red: 0.000, green: 0.000, blue: 0.502, alpha: 1)
+    public static let primitiveColorOlive = UIColor(red: 0.502, green: 0.502, blue: 0.000, alpha: 1)
+    public static let primitiveColorPrimary = UIColor(red: 0.000, green: 0.439, blue: 0.953, alpha: 1)
+    public static let primitiveColorPurple = UIColor(red: 0.502, green: 0.000, blue: 0.502, alpha: 1)
+    public static let primitiveColorRed = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let primitiveColorSecondary = UIColor(red: 0.110, green: 0.110, blue: 0.118, alpha: 1)
+    public static let primitiveColorSilver = UIColor(red: 0.753, green: 0.753, blue: 0.753, alpha: 1)
+    public static let primitiveColorTeal = UIColor(red: 0.000, green: 0.502, blue: 0.502, alpha: 1)
+    public static let primitiveColorWhite = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let primitiveColorYellow = UIColor(red: 1.000, green: 1.000, blue: 0.000, alpha: 1)
+    public static let semanticColorBackgroundDefault = UIColor(red: 0.000, green: 0.439, blue: 0.953, alpha: 1)
+    public static let semanticColorBackgroundOverlay = UIColor(red: 0.110, green: 0.110, blue: 0.118, alpha: 1)
+    public static let semanticColorBorderDefault = UIColor(red: 0.000, green: 0.439, blue: 0.953, alpha: 1)
+    public static let semanticColorBorderHighlight = UIColor(red: 0.110, green: 0.110, blue: 0.118, alpha: 1)
+    public static let semanticColorElementPrimary = UIColor(red: 0.000, green: 0.439, blue: 0.953, alpha: 1)
+    public static let semanticColorElementSecondary = UIColor(red: 0.110, green: 0.110, blue: 0.118, alpha: 1)
+    public static let semanticColorOverlayDark = UIColor(red: 0.110, green: 0.110, blue: 0.118, alpha: 1)
+    public static let semanticColorOverlayLight = UIColor(red: 0.000, green: 0.439, blue: 0.953, alpha: 1)
+}
---
+++ build/ios/StyleDictionarySize.swift
@@ -0,0 +1,43 @@
+
+//
+// StyleDictionarySize.swift
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+import UIKit
+
+public class StyleDictionarySize {
+    public static let primitiveBorderThick = CGFloat(32.00)
+    public static let primitiveBorderThin = CGFloat(16.00)
+    public static let primitiveRadiusLarge = CGFloat(256.00)
+    public static let primitiveRadiusMedium = CGFloat(128.00)
+    public static let primitiveRadiusSmall = CGFloat(64.00)
+    public static let primitiveSpacingLarge = CGFloat(256.00)
+    public static let primitiveSpacingMedium = CGFloat(128.00)
+    public static let primitiveSpacingSmall = CGFloat(64.00)
+    public static let primitiveTypographyFontSizeBody = CGFloat(256.00)
+    public static let primitiveTypographyFontSizeHeading = CGFloat(384.00)
+    public static let primitiveTypographyFontSizeLarge = CGFloat(352.00)
+    public static let primitiveTypographyFontSizeMedium = CGFloat(288.00)
+    public static let primitiveTypographyFontSizeSmall = CGFloat(224.00)
+    public static let primitiveTypographyFontSizeSubHeading = CGFloat(320.00)
+    public static let primitiveTypographyFontSizeTiny = CGFloat(192.00)
+    public static let primitiveTypographyFontSizeXLarge = CGFloat(448.00)
+    public static let primitiveTypographyFontSizeXxLarge = CGFloat(512.00)
+    public static let semanticBorderDefault = CGFloat(16.00)
+    public static let semanticBorderHighlight = CGFloat(32.00)
+    public static let semanticCornerRadiusLarge = CGFloat(256.00)
+    public static let semanticCornerRadiusSmall = CGFloat(64.00)
+    public static let semanticSpacingMargin = CGFloat(256.00)
+    public static let semanticSpacingPadding = CGFloat(128.00)
+    public static let semanticTypographyActionFontSize = CGFloat(256.00)
+    public static let semanticTypographyBodyFontSize = CGFloat(256.00)
+    public static let semanticTypographyCaptionFontSize = CGFloat(256.00)
+    public static let semanticTypographyHeadlineFontSize = CGFloat(384.00)
+    public static let semanticTypographyLableFontSize = CGFloat(256.00)
+    public static let semanticTypographyOverlineFontSize = CGFloat(256.00)
+    public static let semanticTypographyParagraphFontSize = CGFloat(256.00)
+    public static let semanticTypographyTitleFontSize = CGFloat(384.00)
+}
